525a63d14070b0ae60d016d88798910cd06aa2c8
AndreyGalchevski <andreygalchevski@gmail.com>
2019-10-09 16:14:27 +0300

    change back base url for the api

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,7 +1,6 @@
 import { Credentials, LoginResponse } from './types';
 
-let baseURL = 'https://strident-api.herokuapp.com/api';
-// let baseURL = 'http://localhost:8080/api';
+let baseURL = 'http://localhost:8080/api';
 
 if (process.env.NODE_ENV === 'production') {
   baseURL = 'https://strident-api.herokuapp.com/api';
