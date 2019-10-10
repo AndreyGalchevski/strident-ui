8287587881928c0934fa4a1ec33fd91073f02389
AndreyGalchevski <andreygalchevski@gmail.com>
2019-10-10 07:39:01 +0300

    style members page

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -3,6 +3,17 @@ import React, { useState, useEffect } from 'react';
 import { Member } from '../api/types';
 import { fetchResources } from '../api/utils';
 
+const styles = {
+  card: {
+    // height: '20em',
+    // width: '10em',
+  },
+  cardImage: {
+    // height: '80%',
+    // width: '80%',
+  },
+};
+
 function Members(): React.ReactElement {
   const [members, setMembers] = useState<Member[]>([]);
   const [isLoading, setLoading] = useState(false);
@@ -25,14 +36,18 @@ function Members(): React.ReactElement {
   return (
     <section>
       <h3>Members</h3>
-      <div>
+      <div className="row">
         {members.map(member => (
-          <div key={member._id}>
-            <p>{member._id}</p>
-            <p>{member.name}</p>
-            <p>{member.instrument}</p>
-            <p>{member.info}</p>
-            <p>{member.image}</p>
+          <div key={member._id} className="col s12 m3">
+            <div className="card" style={styles.card}>
+              <div className="card-image">
+                <img src={member.image} alt="" style={styles.cardImage} />
+                <span className="card-title">{member.name}</span>
+              </div>
+              <div className="card-content">
+                <p>{member.instrument}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
