5caafde9a5f5e6fa5dd60661a1160f4b3aebd406
AndreyGalchevski <andreygalchevski@gmail.com>
2019-10-20 15:15:15 +0300

    add alt to home images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,6 +26,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914457/strident/home/1.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914457/strident/home/1.jpg`,
+      originalAlt: 'Strident performing in Kiev',
+      thumbnailAlt: 'Strident performing in Kiev',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571406645/strident/home/1_ng.webp`,
@@ -40,6 +42,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914452/strident/home/2.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914452/strident/home/2.jpg`,
+      originalAlt: 'Strident performing in Kiev',
+      thumbnailAlt: 'Strident performing in Kiev',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410056/strident/home/2_ng.webp`,
@@ -54,6 +58,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914449/strident/home/3.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914449/strident/home/3.jpg`,
+      originalAlt: 'Strident performing in Tel Aviv',
+      thumbnailAlt: 'Strident performing in Tel Aviv',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410056/strident/home/3_ng.webp`,
@@ -68,6 +74,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914452/strident/home/4.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914452/strident/home/4.jpg`,
+      originalAlt: 'Strident performing in Kiev',
+      thumbnailAlt: 'Strident performing in Kiev',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410058/strident/home/4_ng.webp`,
@@ -82,6 +90,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914455/strident/home/5.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914455/strident/home/5.jpg`,
+      originalAlt: 'Strident performing in Kiev',
+      thumbnailAlt: 'Strident performing in Kiev',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410055/strident/home/5_ng.webp`,
@@ -96,6 +106,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914457/strident/home/6.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914457/strident/home/6.jpg`,
+      originalAlt: 'Strident performing in Tel Aviv',
+      thumbnailAlt: 'Strident performing in Tel Aviv',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410058/strident/home/6_ng.webp`,
@@ -110,6 +122,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914457/strident/home/7.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914457/strident/home/7.jpg`,
+      originalAlt: 'Strident performing in Kiev',
+      thumbnailAlt: 'Strident performing in Kiev',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410057/strident/home/7_ng.webp`,
@@ -124,6 +138,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914451/strident/home/8.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914451/strident/home/8.jpg`,
+      originalAlt: 'Strident performing in Kiev',
+      thumbnailAlt: 'Strident performing in Kiev',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410056/strident/home/8_ng.webp`,
@@ -138,6 +154,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914456/strident/home/9.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914456/strident/home/9.jpg`,
+      originalAlt: 'Strident performing in Kiev',
+      thumbnailAlt: 'Strident performing in Kiev',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410058/strident/home/9_ng.webp`,
@@ -152,6 +170,8 @@ function Home(): ReactElement {
     {
       original: `${baseURL}/v1570914452/strident/home/10.jpg`,
       thumbnail: `${baseURL}/c_thumb,w_200,g_face/v1570914452/strident/home/10.jpg`,
+      originalAlt: 'Strident performing in Tel Aviv',
+      thumbnailAlt: 'Strident performing in Tel Aviv',
       imageSet: [
         {
           srcSet: `${baseURL}/v1571410057/strident/home/10_ng.webp`,
